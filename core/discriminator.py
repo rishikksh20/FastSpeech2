ae7bf27417a8d418b8cc2a37f3ec297c374a0075
rishikksh20 <rishikksh20@gmail.com>
2021-01-03 03:08:06 +0530

    :books: Modified Discriminator

--- core/discriminator.py
+++ core/discriminator.py
@@ -1,72 +1,81 @@
 import torch.nn as nn
 from utils.util import weights_init
 
+
+class Dblock(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim = 256):
+        super(Dblock, self).__init__()
+        self.input_spec = nn.utils.weight_norm(nn.Conv1d(input_dim, output_dim, kernel_size=3, stride=1))
+        self.input_cond = nn.utils.weight_norm(nn.Conv1d(hidden_dim, output_dim, kernel_size=3, stride=1))
+
+        self.output_spec = nn.Sequential(
+            nn.LeakyReLU(0.2, True),
+            nn.utils.weight_norm(nn.Conv1d(output_dim, output_dim, kernel_size=3, stride=1)),
+        )
+
+        self.output_cond = nn.Sequential(
+            nn.LeakyReLU(0.2, True),
+            nn.utils.weight_norm(nn.Conv1d(output_dim, hidden_dim, kernel_size=3, stride=1)),
+        )
+
+    def forward(self, spec, cond):
+        x1 = self.input_spec(spec)
+        y1 = self.input_cond(cond)
+        x = x1 + y1
+        out1 = self.output_spec(x)
+        out2 = self.output_cond(y1)
+        return out1 + x1, out2 + cond
+
 class Discriminator(nn.Module):
-    def __init__(self, ndf = 16, n_layers = 3, downsampling_factor = 4, disc_out = 512):
+    def __init__(self, nf = 80, n_layers = 4, hidden_dim = 256):
         super(Discriminator, self).__init__()
         discriminator = nn.ModuleDict()
-        discriminator["layer_0"] = nn.Sequential(
-            nn.ReflectionPad1d(7),
-            nn.utils.weight_norm(nn.Conv1d(1, ndf, kernel_size=15, stride=1)),
-            nn.LeakyReLU(0.2, True),
-        )
 
-        nf = ndf
-        stride = downsampling_factor
         for n in range(1, n_layers + 1):
-            nf_prev = nf
-            nf = min(nf * stride, disc_out)
+            discriminator["layer_%d" % n] = Dblock(nf, nf*2, hidden_dim)
+            nf = nf * 2
 
-            discriminator["layer_%d" % n] = nn.Sequential(
-                nn.utils.weight_norm(nn.Conv1d(
-                    nf_prev,
-                    nf,
-                    kernel_size=stride * 10 + 1,
-                    stride=stride,
-                    padding=stride * 5,
-                    groups=nf_prev // 4,
-                )),
-                nn.LeakyReLU(0.2, True),
-            )
-        nf = min(nf * 2, disc_out)
-        discriminator["layer_%d" % (n_layers + 1)] = nn.Sequential(
-            nn.utils.weight_norm(nn.Conv1d(nf, disc_out, kernel_size=5, stride=1, padding=2)),
-            nn.LeakyReLU(0.2, True),
-        )
+        self.disc_spec = nn.utils.weight_norm(nn.Conv1d(nf, 1, kernel_size=3, stride=1, padding=1))
 
-        discriminator["layer_%d" % (n_layers + 2)] = nn.utils.weight_norm(nn.Conv1d(
-            nf, 1, kernel_size=3, stride=1, padding=1
+
+        self.disc_cond = nn.utils.weight_norm(nn.Conv1d(
+            hidden_dim, 1, kernel_size=3, stride=1, padding=1
         ))
         self.discriminator = discriminator
 
-    def forward(self, x):
+    def forward(self, x, y):
         '''
             returns: (list of 6 features, discriminator score)
             we directly predict score without last sigmoid function
             since we're using Least Squares GAN (https://arxiv.org/abs/1611.04076)
         '''
         features = list()
+
         for key, module in self.discriminator.items():
-            x = module(x)
+            x, y = module(x, y)
             features.append(x)
+        out  = self.disc_spec(x) + self.disc_cond(y)
+        features.append(out)
         return features[:-1], features[-1]
 
+
 class MultiScaleDiscriminator(nn.Module):
-    def __init__(self, num_D = 3, ndf = 16, n_layers = 3, downsampling_factor = 4, disc_out = 512):
+    def __init__(self, num_D = 3, ndf = 80, n_layers = 4, downsampling_factor = 2, hidden_dim = 256):
         super().__init__()
         self.model = nn.ModuleDict()
         for i in range(num_D):
             self.model[f"disc_{i}"] = Discriminator(
-                ndf, n_layers, downsampling_factor, disc_out
+                ndf, n_layers, hidden_dim
             )
 
         self.downsample = nn.AvgPool1d(downsampling_factor, stride=2, padding=1, count_include_pad=False)
         self.apply(weights_init)
 
-    def forward(self, x):
+    def forward(self, x, y):
         results = []
         for key, disc in self.model.items():
-            results.append(disc(x))
+            results.append(disc(x, y))
             x = self.downsample(x)
+            y = self.downsample(y)
         return results
 
